b0df41fbe722e1b83231c18ab4f44c2033bac84e
GEOVANI\conta <geosegat@gmail.com>
2024-08-17 15:27:32 -0300

    alterada a cor do icon de localizacao e definindo um tamanho fixo para o icon caretdown

--- src/Components/StateSelection.tsx
+++ src/Components/StateSelection.tsx
@@ -9,11 +9,11 @@ const StateSelection: React.FC<StateSelectionProps> = ({ label }) => {
   return (
     <a href="">
       <div className="flex flex-row items-center">
-        <Localizacao color="white" />
+        <Localizacao color="red" />
         <h1 className="text-red-500 font-bold font-sans ml-0.5">
           {label ?? "ELDORADO"}
         </h1>
-        <CaretDown className="ml-1.5" color="white" />
+        <CaretDown width={20} height={20} className="ml-1.5" color="white" />
       </div>
     </a>
   );
