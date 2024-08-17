cb9463d776d48ba45fbdb1a31877039443cf9dbb
GEOVANI\conta <geosegat@gmail.com>
2024-08-17 15:26:42 -0300

    importado o novo componente ao app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import HeaderMenu from "./Components/HeaderMenu";
 import StateSelection from "./Components/StateSelection";
+import SearchMovies from "./Components/SearchMovies";
+import { IconUser } from "./assets/Icons";
 
 function App() {
   return (
     <div className="bg-black h-screen ">
       <div className="flex flex-row items-center">
         <HeaderMenu />
-        <StateSelection />
+        <StateSelection label="São Paulo" />
+        <SearchMovies />
+        <IconUser width={20} height={20} className="ml-3" color="white" />
       </div>
     </div>
   );
