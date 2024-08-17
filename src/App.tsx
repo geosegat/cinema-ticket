bf8481c8285eea9497f78b4531ce909c5320d985
GEOVANI\conta <geosegat@gmail.com>
2024-08-17 16:16:34 -0300

    card importado ap app.tsx e itens centralizados na tela

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import HeaderMenu from "./Components/HeaderMenu";
 import StateSelection from "./Components/StateSelection";
 import SearchMovies from "./Components/SearchMovies";
 import { IconUser } from "./assets/Icons";
+import MovieShowcase from "./Components/MovieShowcase";
 
 function App() {
   return (
-    <div className="bg-black h-screen ">
+    <div className="bg-black h-screen flex flex-col  items-center">
       <div className="flex flex-row items-center">
         <HeaderMenu />
         <StateSelection label="São Paulo" />
         <SearchMovies />
         <IconUser width={20} height={20} className="ml-3" color="white" />
       </div>
+      <MovieShowcase />
     </div>
   );
 }
