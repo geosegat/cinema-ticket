8fcf1d5e67f31e0fdb66421d62815b69a00a8956
GEOVANI\conta <geosegat@gmail.com>
2024-08-19 15:48:06 -0300

    removido alguns botoes

--- src/Components/MovieShowcase.tsx
+++ src/Components/MovieShowcase.tsx
@@ -3,6 +3,7 @@ import backgroundImage1 from "../assets/images/banner1.jpg";
 import backgroundImage2 from "../assets/images/banner2.jpg";
 import backgroundImage3 from "../assets/images/banner3.jpg";
 import backgroundImage4 from "../assets/images/banner4.jpg";
+import CardButton from "./CardButton";
 
 const images = [
   backgroundImage1,
@@ -25,36 +26,14 @@ const MovieShowcase: React.FC = () => {
   };
 
   return (
-    <div
-      className="relative bg-cover bg-center w-full h-64 md:h-80 lg:h-96"
-      style={{
-        backgroundImage: `url(${images[currentIndex]})`,
-        borderRadius: 20,
-      }}
-    >
-      <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-center p-4">
-        <h1 className="text-white text-2xl md:text-3xl lg:text-4xl font-bold">
-          Filme em Destaque
-        </h1>
-        <p className="text-white text-sm md:text-base lg:text-lg mt-2 md:mt-4">
-          Descrição ou tagline do filme.
-        </p>
-        <button className="mt-4 px-4 py-2 md:px-6 md:py-3 bg-red-500 text-white font-semibold rounded">
-          Comprar Ingresso
-        </button>
-      </div>
-      <button
-        className="absolute left-2 md:left-4 top-1/2 transform -translate-y-1/2 p-2 md:p-4 bg-gray-800 bg-opacity-50 text-white rounded-full"
-        onClick={handlePrevious}
-      >
-        &lt;
-      </button>
-      <button
-        className="absolute right-2 md:right-4 top-1/2 transform -translate-y-1/2 p-2 md:p-4 bg-gray-800 bg-opacity-50 text-white rounded-full"
-        onClick={handleNext}
-      >
-        &gt;
-      </button>
+    <div className="relative w-full h-64 md:h-80 lg:h-96 overflow-hidden">
+      <img
+        src={images[currentIndex]}
+        alt="Movie Banner"
+        className="object-cover w-full h-full"
+      />
+      <CardButton onPressLeft={handlePrevious} />
+      <CardButton onPressRight={handleNext} right />
     </div>
   );
 };
