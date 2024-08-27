68833255c88d3dc46bd3805f135ab53ee767245d
GEOVANI\conta <geosegat@gmail.com>
2024-08-27 08:41:18 -0300

    alterado nome do componente  para SearchMovieButton

---
+++ src/Components/SearchMovieButton .tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { IconSearch } from "../assets/Icons/index";
+
+interface SearchMovieProps {
+  onClick?: () => void;
+}
+
+const SearchMovies: React.FC<SearchMovieProps> = ({ onClick }) => {
+  return (
+    <button onClick={onClick} className="flex flex-row items-center">
+      <IconSearch width={20} height={20} color="white" />
+      <h1 className="text-white font-bold">ENCONTRE UM FILME</h1>
+    </button>
+  );
+};
+
+export default SearchMovies;
