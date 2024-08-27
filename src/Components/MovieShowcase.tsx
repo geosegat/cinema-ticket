80c8e66b014caab82a7b56b8d8f2ab6686138ed7
GEOVANI\conta <geosegat@gmail.com>
2024-08-27 08:42:12 -0300

    add items pela api

--- src/Components/MovieShowcase.tsx
+++ src/Components/MovieShowcase.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { fetchMovies } from "./movieServices";
 import CardButton from "./CardButton";
 import ButtonBuyTicket from "./ButtonBuyTicket";
 
-const auth =
-  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMzI5ZDZkOTE4YjEwYjIxMzE0ZDcyNzk5ZDc1ZWVmYyIsIm5iZiI6MTcyNDM3MzYzOS44MzYzOTksInN1YiI6IjY2YmI4YzZkYjBmYWRhMzg1MGMzMzEwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EU_dH40JMVAAObYWuHGk6OHSdix8r7k0JGgdvMzfH8s";
 const baseUrl = "https://image.tmdb.org/t/p/";
 const size = "original";
 
@@ -23,31 +21,16 @@ const MovieShowcase: React.FC = () => {
   );
 
   useEffect(() => {
-    const fetchMovies = async () => {
+    const loadMovies = async () => {
       try {
-        const response = await axios.get(
-          "https://api.themoviedb.org/3/discover/movie",
-          {
-            params: {
-              include_adult: false,
-              include_video: false,
-              language: "pt-BR",
-              page: 2,
-              sort_by: "popularity.desc",
-            },
-            headers: {
-              accept: "application/json",
-              Authorization: auth,
-            },
-          }
-        );
-        setMovies(response.data.results.slice(0, 7));
+        const results = await fetchMovies(1);
+        setMovies(results.slice(0, 15));
       } catch (error) {
-        console.error("Erro ao buscar filmes:", error);
+        console.error("Erro ao carregar filmes:", error);
       }
     };
 
-    fetchMovies();
+    loadMovies();
   }, []);
 
   useEffect(() => {
@@ -67,7 +50,7 @@ const MovieShowcase: React.FC = () => {
   const currentMovie = movies[currentIndex];
 
   return (
-    <div className="relative w-full h-64 sm:h-72 md:h-80 lg:h-96 overflow-hidden">
+    <div className="relative w-full h-64 sm:h-72 md:h-80 lg:h-96 overflow-hidden rounded-xl mb-10">
       {movies.length > 0 && (
         <div
           className={`flex ${transitionClass} absolute w-full h-full`}
@@ -81,7 +64,7 @@ const MovieShowcase: React.FC = () => {
               <img
                 src={`${baseUrl}${size}${movie.backdrop_path}`}
                 alt={movie.title}
-                className="object-cover w-full h-full rounded-xl"
+                className="object-contain "
               />
             </div>
           ))}
