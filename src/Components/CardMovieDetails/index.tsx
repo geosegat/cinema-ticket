4ad9a1cc217c92590116bc96fde61d664e95d5aa
GEOVANI\conta <geosegat@gmail.com>
2024-08-29 15:35:45 -0300

    alterado para o componente e removido os h1 repetidos

--- src/Components/CardMovieDetails/index.tsx
+++ src/Components/CardMovieDetails/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CardMovieInfoRow from "../CardMovieInfoRow";
 
 const dadosMock = {
   dataDeEstreia: "29.08.2024",
@@ -12,27 +13,27 @@ const dadosMock = {
 
 const CardMovieDetails = () => {
   return (
-    <div className="bg-pink-800">
+    <div className="max-w-lg max-h-full ">
       <div className="w-56 h-56 bg-yellow-600"></div>
       <img className="rounded-lg shadow-lg" />
       <h1 className="text-white">18 Terror</h1>
-      <h1 className="text-white">
-        Aagente do FBI Lee Harker (Maika Monroe) é designada para um caso
+      <h1 className="text-white"></h1>
+      <div className="">
+        <CardMovieInfoRow
+          value=" Aagente do FBI Lee Harker (Maika Monroe) é designada para um caso
         envolvendo um serial killer impiedoso que se autointitula LONGLEGS
         (Nicolas Cage). O assassino costuma deixar pistas com símbolos nas cenas
         do crime que apenas a agente é capaz de decifrar. A investigação toma
         rumos inesperados, enquanto Harker descobre uma conexão macabra com o
         assassino. Classificação indicativa 18 Anos. Contém drogas lícitas,
-        medo, violência extrema.
-      </h1>
-      <h1 className="text-white">Data de estreia: {dadosMock.dataDeEstreia}</h1>
-      <h1 className="text-white">
-        Distribuido por: {dadosMock.distribuidoPor}
-      </h1>
-      <h1 className="text-white">Acessibilidade: {dadosMock.acessibilidade}</h1>
+        medo, violência extrema."
+        />
+
+        <CardMovieInfoRow label="Diretor" value="Tomar no cu" />
+        <CardMovieInfoRow label="Data" value="20.05.1995" />
+        <CardMovieInfoRow label="Diretor" value="Tomar no cu" />
+      </div>
       <p className="text-white text-xl font-bold">Elenco e Equipe</p>
-      <h1 className="text-white">Diretor: {dadosMock.elencoEquipe.diretor}</h1>
-      <h1 className="text-white">Elenco: {dadosMock.elencoEquipe.elenco}</h1>
     </div>
   );
 };
