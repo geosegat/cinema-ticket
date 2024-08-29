de86ff0897863e52e8fe52e7dd91924bab082f66
GEOVANI\conta <geosegat@gmail.com>
2024-08-29 20:51:37 -0300

    import do cardSessionINfo; add interfaceprops;

--- src/Components/CardShowTime/index.tsx
+++ src/Components/CardShowTime/index.tsx
@@ -1,19 +1,26 @@
 import React from "react";
+import CardSessionInfo from "../CardSessionInfo";
 
-interface CardDropDownProps {}
+interface CardDropDownProps {
+  classname?: string;
+}
 
 const dates = [
   ["Hoje, 28 Ago", "Qui, 29 Ago", "Sex, 30 Ago", "Sab, 31 Ago", "Dom, 01 Set"],
 ];
 
-const CardShowTime: React.FC<CardDropDownProps> = () => {
+const CardShowTime: React.FC<CardDropDownProps> = ({ classname }) => {
   return (
-    <button
-      className="bg-gray-700  min-h-[160px] 
-     text-white font-bold text-sm flex justify-center pt-5 rounded-lg px-5"
-    >
-      Qui, 29 Ago, 19:30
-    </button>
+    <div className={classname}>
+      <CardSessionInfo language="DUBLADO" room="SALA 2" sessionType="3D" />
+      <button
+        className="bg-gray-700  min-h-[160px] 
+     text-white font-bold text-sm flex justify-center pt-5 rounded-lg px-5
+     mt-5"
+      >
+        Qui, 29 Ago, 19:30
+      </button>
+    </div>
   );
 };
 
